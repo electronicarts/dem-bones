37e9ffe4ce1230428931e6bace5a6d18601b16be
Binh Le <binh@lax1--BLE-L.la.ad.ea.com>
2020-06-22 09:21:20 -0700

    v1.1.0

--- cmake/FindAlembic.cmake
+++ cmake/FindAlembic.cmake
@@ -13,7 +13,7 @@ if(WIN32)
 		PATHS "ExtLibs/Alembic/lib"
 	)
 	file(GLOB ALEMBIC_LIBS "${ALEMBIC_LIB_DIR}/*.lib")
-elseif()
+else()
 	find_path(ALEMBIC_LIB_DIR
 		NAMES "libAlembic.a"
 		PATHS "ExtLibs/Alembic/lib"
